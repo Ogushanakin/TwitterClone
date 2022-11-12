6d2bce47239d119cb199d627ce3c3ae205f109e1
Ogushanakin <ogzhnakn96@gmail.com>
2022-11-12 16:01:18 +0300

    Upload User Data and Refactor API Code Into Service Class

---
+++ TwitterClone/TwitterClone/Core/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  TwitterClone
+//
+//  Created by AKIN on 11.11.2022.
+//
+
+import Firebase
+import FirebaseStorage
+
+let STORAGE_REF = Storage.storage().reference()
+let STORAGE_PROFILE_IMAGES = STORAGE_REF.child("profile_images")
+
+let DB_REF = Database.database().reference()
+let REF_USERS = DB_REF.child("users")
--- TwitterClone/TwitterClone/Screen/RegistrationScreen/RegistrationController.swift
+++ TwitterClone/TwitterClone/Screen/RegistrationScreen/RegistrationController.swift
@@ -13,6 +13,7 @@ class RegistrationController: UIViewController {
     // MARK: - Properties
     
     private let imagePicker = UIImagePickerController()
+    private var profileImage: UIImage?
     
     private lazy var plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
@@ -105,16 +106,19 @@ class RegistrationController: UIViewController {
     }
     
     @objc func handleRegistration() {
+        guard let profileImage = profileImage else {
+            print("DEBUG: Please select a profile image...")
+            return
+        }
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
+        guard let fullname = fullnameTextField.text else { return }
+        guard let username = usernameTextField.text else { return }
         
-        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
-            if let error = error {
-                print("DEBUG: Error is \(error.localizedDescription)")
-                return
-            }
-            
-            print("DEBUG: Successfully registered user")
+        let credentials = AuthCredentials(email: email, password: password, fullname: fullname, username: username, profileImage: profileImage)
+        
+        AuthService.shared.registerUser(credentials: credentials) { (error, ref) in
+            print("DEBUG: Sign Up Succesfully..")
         }
     }
     
@@ -165,6 +169,7 @@ extension RegistrationController: UIImagePickerControllerDelegate, UINavigationC
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let profileImage = info[.editedImage] as? UIImage else { return }
+        self.profileImage = profileImage
         
         plusPhotoButton.layer.cornerRadius = 128 / 2
         plusPhotoButton.layer.masksToBounds = true
