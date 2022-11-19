8c8fd9686dc289f2daf49bd0953e8876bf3c64ec
Ogushanakin <ogzhnakn96@gmail.com>
2022-11-19 20:31:12 +0300

    Profile Header View Model

--- TwitterClone/TwitterClone/Core/ViewModel/ProfileHeaderViewModel.swift
+++ TwitterClone/TwitterClone/Core/ViewModel/ProfileHeaderViewModel.swift
@@ -5,7 +5,7 @@
 //  Created by AKIN on 19.11.2022.
 //
 
-import Foundation
+import UIKit
 
 enum ProfileFilterOptions: Int, CaseIterable {
     case tweets
@@ -18,5 +18,39 @@ enum ProfileFilterOptions: Int, CaseIterable {
         case .replies: return "Tweets & Replies"
         case .likes: return "Likes"
         }
+    }
+}
+
+
+struct ProfileHeaderViewModel {
+    
+    private let user: User
+    
+    var followersString: NSAttributedString? {
+        return attributedText(withValue: 0, text: "followers")
+    }
+    
+    var followingString: NSAttributedString? {
+        return attributedText(withValue: 2, text: "following")
+    }
+    
+    var actionButtonTitle: String {
+        if user.isCurrentUser {
+            return "Edit Profile"
+        } else {
+            return "Follow"
+        }
+    }
+    
+    init(user: User) {
+        self.user = user
+    }
+    
+    func attributedText(withValue value: Int, text: String) -> NSAttributedString {
+        let attributedTitle = NSMutableAttributedString(string: "\(value)",
+                                                        attributes: [.font : UIFont.boldSystemFont(ofSize: 14)])
+        attributedTitle.append(NSAttributedString(string: " \(text)",
+                                                  attributes: [.font: UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.lightGray]))
+        return attributedTitle
     }
 }
