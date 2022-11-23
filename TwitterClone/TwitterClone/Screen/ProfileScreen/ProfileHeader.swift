69bcf39234d14848813d473abe78e488d9075f51
Ogushanakin <ogzhnakn96@gmail.com>
2022-11-23 20:50:37 +0300

    Following User

--- TwitterClone/TwitterClone/Screen/ProfileScreen/ProfileHeader.swift
+++ TwitterClone/TwitterClone/Screen/ProfileScreen/ProfileHeader.swift
@@ -9,6 +9,7 @@ import UIKit
 
 protocol ProfileHeaderDelegate: AnyObject {
     func handleDismissal()
+    func handleEditProfileFollow(_ header: ProfileHeader)
 }
 
 final class ProfileHeader: UICollectionReusableView {
@@ -174,7 +175,7 @@ final class ProfileHeader: UICollectionReusableView {
     }
     
     @objc func handleEditProfileFollow() {
-        
+        delegate?.handleEditProfileFollow(self)
     }
     
     @objc func handleFollowersTapped() {
