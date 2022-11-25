85f3bd7fda0ad044fb91c210288039255ff0b579
Ogushanakin <ogzhnakn96@gmail.com>
2022-11-25 16:00:03 +0300

    Check If User Is Followed In Database

--- TwitterClone/TwitterClone/Screen/ProfileScreen/ProfileController.swift
+++ TwitterClone/TwitterClone/Screen/ProfileScreen/ProfileController.swift
@@ -35,6 +35,7 @@ final class ProfileController: UICollectionViewController {
         super.viewDidLoad()
         configureCollectionView()
         fetchTweets()
+        checkIfUserIsFollowed()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -48,6 +49,13 @@ final class ProfileController: UICollectionViewController {
     func fetchTweets() {
         TweetService.shared.fetchTweets(forUser: user) { tweets in
             self.tweets = tweets
+        }
+    }
+    
+    func checkIfUserIsFollowed() {
+        UserService.shared.checkIfUserIsFollowed(uid: user.uid) { isFollowed in
+            self.user.isFollowed = isFollowed
+            self.collectionView.reloadData()
         }
     }
     
@@ -107,19 +115,15 @@ extension ProfileController: UICollectionViewDelegateFlowLayout {
 extension ProfileController: ProfileHeaderDelegate {
     func handleEditProfileFollow(_ header: ProfileHeader) {
         
-        print("DEBUG: User is followed is \(user.isFollowed) before button tap")
-        
-        user.isFollowed.toggle()
-        
         if user.isFollowed {
             UserService.shared.unfollowUser(uid: user.uid) { (ref, err) in
                 self.user.isFollowed = false
-                print("DEBUG: User is followed is \(self.user.isFollowed) after button tap")
+                self.collectionView.reloadData()
             }
         } else {
             UserService.shared.followUser(uid: user.uid) { (ref, err) in
                 self.user.isFollowed = true
-                print("DEBUG: User is followed is \(self.user.isFollowed) after button tap")
+                self.collectionView.reloadData()
             }
         }
     }
