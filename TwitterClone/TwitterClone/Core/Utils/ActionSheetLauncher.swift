9a67092084e346a51256e06eda60a5c5a2ff6839
Ogushanakin <ogzhnakn96@gmail.com>
2022-12-01 09:11:50 +0300

    Action Sheet Data Model + View Model

--- TwitterClone/TwitterClone/Core/Utils/ActionSheetLauncher.swift
+++ TwitterClone/TwitterClone/Core/Utils/ActionSheetLauncher.swift
@@ -16,6 +16,7 @@ final class ActionSheetLauncher: NSObject {
     private let user: User
     private let tableView = UITableView()
     private var window: UIWindow?
+    private lazy var viewModel = ActionSheetViewModel(user: user)
     
     private lazy var blackView: UIView = {
         let view = UIView()
@@ -79,7 +80,7 @@ final class ActionSheetLauncher: NSObject {
         blackView.frame = window.frame
         
         window.addSubview(tableView)
-        let height = CGFloat(3 * 60) + 100
+        let height = CGFloat(viewModel.option.count * 60) + 100
         tableView.frame = CGRect(x: 0, y: window.frame.height,
                                  width: window.frame.width, height: 300)
         
@@ -104,11 +105,12 @@ final class ActionSheetLauncher: NSObject {
 
 extension ActionSheetLauncher: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return viewModel.option.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! ActionSheetCell
+        cell.option = viewModel.option[indexPath.row]
         return cell
     }
     
