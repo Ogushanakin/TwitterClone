c4fa43bd46eb8729f9a551db8e9beffb705a4811
Ogushanakin <ogzhnakn96@gmail.com>
2022-12-04 07:38:43 +0300

    Upload Like Notification

---
+++ TwitterClone/TwitterClone/Core/API/NotificationService.swift
@@ -0,0 +1,27 @@
+//
+//  NotificationService.swift
+//  TwitterClone
+//
+//  Created by AKIN on 4.12.2022.
+//
+
+import Firebase
+
+struct NotificationService {
+    static let shared = NotificationService()
+    
+    func uploadNotification(type: NotificationType, tweet: Tweet? = nil) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        var values: [String: Any] = ["timestamp": Int(NSDate().timeIntervalSince1970),
+                                     "uid": uid,
+                                     "type": type.rawValue]
+        
+        if let tweet = tweet {
+            values["tweetID"] = tweet.tweetID
+            REF_NOTIFICATIONS.child(tweet.user.uid).childByAutoId().updateChildValues(values)
+        } else {
+            
+        }
+    }
+}
