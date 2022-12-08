0235ef823b5dec481e12b5fbef18e385a1bc7a78
Ogushanakin <ogzhnakn96@gmail.com>
2022-12-08 06:32:01 +0300

    Filter View Refactor

--- TwitterClone/TwitterClone/Screen/ProfileScreen/ProfileHeader.swift
+++ TwitterClone/TwitterClone/Screen/ProfileScreen/ProfileHeader.swift
@@ -82,12 +82,6 @@ final class ProfileHeader: UICollectionReusableView {
         return label
     }()
     
-    private let underlineView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .twitterBlue
-        return view
-    }()
-    
     private lazy var followingLabel: UILabel = {
         let label = UILabel()
         
@@ -159,9 +153,6 @@ final class ProfileHeader: UICollectionReusableView {
         filterBar.anchor(left: leftAnchor, bottom: bottomAnchor,
                          right: rightAnchor, height: 50)
         
-        addSubview(underlineView)
-        underlineView.anchor(left: leftAnchor, bottom: bottomAnchor,
-                             width: frame.width / 3, height: 2)
     }
     
     required init?(coder: NSCoder) {
@@ -208,11 +199,7 @@ final class ProfileHeader: UICollectionReusableView {
 
 extension ProfileHeader: ProfileFilterViewDelegate {
     func filterView(_ view: ProfileFilterView, didSelect indexPath: IndexPath) {
-        guard let cell = view.collectionView.cellForItem(at: indexPath) as? ProfileFilterCell else { return }
         
-        let xPosition = cell.frame.origin.x
-        UIView.animate(withDuration: 0.3) {
-            self.underlineView.frame.origin.x = xPosition
-        }
     }
 }
+
